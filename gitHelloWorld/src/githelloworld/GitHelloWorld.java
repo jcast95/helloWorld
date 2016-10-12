07b16757695890dd3ae0a3f7f84aba641d2ef824
jgcastillo0 <jgcastillo0@CC270-86290.frostburg.edu>
2016-10-12 09:43:24 -0400

    second commit/ doesnt work doesnt read file

--- gitHelloWorld/src/githelloworld/GitHelloWorld.java
+++ gitHelloWorld/src/githelloworld/GitHelloWorld.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package githelloworld;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,8 +21,31 @@ public class GitHelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        System.out.println("Hello World");
+       ArrayList<String> lines = new ArrayList<String>();
+       System.out.println("Hello World");
+       
+       try{
+       FileReader fr = new FileReader(args[0]);
+       BufferedReader br = new BufferedReader(fr);
+       String line = ""; 
+       while((line = br.readLine()) != null)
+       {
+           lines.add(line);
+       }
+       for(String s: lines)
+       {
+           System.out.printf("  -->%S%n", s);
+       }
+       }
+       
+       catch(FileNotFoundException e)
+       {
+           System.out.println("File not found");
+       }
+       catch(IOException c)
+       {
+           System.out.println("IO exception");
+       }
     }
     
 }
